1885f5036e67a3d477537f53013c2acd48dbccf1
Cailean Finn <Caileannn@gmail.com>
2022-11-05 21:11:02 +0000

    Stakcs Wallet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes} from 'react-router-dom';
 import Ruhi from './components/Ruhi/Ruhi';
 import Home from './home/Home';
 
+import * as MicroStacks from '@micro-stacks/react';
+
+
+
 
 
 function App() {
@@ -10,8 +14,7 @@ function App() {
   
   return (
     <>
-      
-        <Routes>
+      <Routes>
           <Route path="/*" element={<Home/>} />
           <Route path="/ruhi" element={<Ruhi />} />
       </Routes>
